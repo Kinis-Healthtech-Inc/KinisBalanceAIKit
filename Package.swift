777529ec30d395e8fe7107df589e318243f6622f
macbook <hoangcuong9331@gmail.com>
2026-03-13 17:56:30 +0700

    fix linking issue

--- Package.swift
+++ Package.swift
@@ -2,32 +2,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "KinisAIKitt",
+    name: "KinisBalanceAIKit",
     platforms: [
         .iOS(.v16)
     ],
     products: [
         .library(
-            name: "KinisAIKitt",
-            targets: ["KinisAIKitt"]
+            name: "KinisBalanceAIKit",
+            targets: ["KinisBalanceAIKit"]
         )
-    ],
-    dependencies: [
-        .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack", exact: "3.9.0"),
-        .package(url: "https://github.com/Alamofire/Alamofire.git", exact: "5.11.1")
     ],
     targets: [
         .binaryTarget(
             name: "KinisBalanceAIKit",
             path: "KinisBalanceAIKit.xcframework",
-        ),
-        .target(
-            name: "KinisAIKitt",
-            dependencies: [
-                "KinisBalanceAIKit",
-                .product(name: "Alamofire", package: "Alamofire"),
-                .product(name: "CocoaLumberjack", package: "CocoaLumberjack")
-            ]
         )
     ]
 )
--- Sources/KinisAIKitt/KinisAIKitt.swift
+++
@@ -1,3 +0,0 @@
-// This file intentionally exists to allow SPM to compile this target
-// and forward dependencies to consumer apps.
-@_exported import KinisBalanceAIKit
