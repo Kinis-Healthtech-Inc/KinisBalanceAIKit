347a8a384c0aa7c5747c46c7d7c1959c40e60c74
macbook <hoangcuong9331@gmail.com>
2026-03-13 15:25:49 +0700

    fix linking issue

--- Package.swift
+++ Package.swift
@@ -12,10 +12,24 @@ let package = Package(
             targets: ["KinisBalanceAIKit"]
         )
     ],
+    dependencies: [
+        .package(url: "git@github.com:HoangCuong9331/DEToolkit.git", from: "2.0.5"),
+        .package(url: "https://github.com/CocoaLumberjack/CocoaLumberjack", from: "3.9.0"),
+        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.11.1")
+    ],
     targets: [
         .binaryTarget(
+            name: "KinisBalanceAIKit-Binary",
+            path: "KinisBalanceAIKit.xcframework",
+        ),
+        .target(
             name: "KinisBalanceAIKit",
-            path: "KinisBalanceAIKit.xcframework"
+            dependencies: [
+                "KinisBalanceAIKit-Binary",
+                .product(name: "Alamofire", package: "Alamofire"),
+                .product(name: "DEToolkit", package: "DEToolkit"),
+                .product(name: "CocoaLumberjack", package: "CocoaLumberjack")
+            ]
         )
     ]
 )
